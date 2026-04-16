f82bb54c56a2642996f09c9622ebf0361aaa2c8c
Ella <ella.moreton20@gmail.com>
2026-04-16 09:56:41 +0000

    Change JS from DOMContentLoaded event to being in a defer script tag

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,12 +1,12 @@
-// enhancement to load/store
-function lightToggle(e) {
-    let value = e.target.checked;
-    const media = window.matchMedia("(prefers-color-scheme: dark)");
-    value ^= media.matches;
-    window.localStorage.setItem("dark-mode", Boolean(value).toString());
-}
+(() => {
+    // enhancement to load/store
+    function lightToggle(e) {
+        let value = e.target.checked;
+        const media = window.matchMedia("(prefers-color-scheme: dark)");
+        value ^= media.matches;
+        window.localStorage.setItem("dark-mode", Boolean(value).toString());
+    }
 
-document.addEventListener("DOMContentLoaded", () => {
     const el = document.getElementById("light");
 
     const media = window.matchMedia("(prefers-color-scheme: dark)");
