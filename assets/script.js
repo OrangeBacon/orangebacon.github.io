3432efecda32d226eca0027e0255077677b696b4
Ella <ella.moreton20@gmail.com>
2026-04-22 10:12:00 +0000

    Deploy

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+(() => {
+    // enhancement to load/store
+    function lightToggle(e) {
+        let value = e.target.checked;
+        const media = window.matchMedia("(prefers-color-scheme: dark)");
+        value ^= media.matches;
+        window.localStorage.setItem("dark-mode", Boolean(value).toString());
+    }
+
+    const el = document.getElementById("light");
+
+    const media = window.matchMedia("(prefers-color-scheme: dark)");
+    const stored = window.localStorage.getItem("dark-mode");
+    if (stored != null) {
+        el.checked = (stored === "true") ^ media.matches;
+    }
+
+    el.addEventListener("change", lightToggle);
+})();
